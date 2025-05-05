d70738a10126a8f992dc4dba311d7917c03dc85b
Absolomjr <145746728+Absolomjr@users.noreply.github.com>
2025-05-05 22:34:48 +0300

    Header

--- daystar_frontend/src/pages/BabysitterManagement.jsx
+++ daystar_frontend/src/pages/BabysitterManagement.jsx
@@ -10,7 +10,7 @@ const BabysitterManagement = () => {
 
   return (
     <div className="babysitter-management">
-      {/* Page Header */}
+      {/* The Page Header */}
       <div className="page-header">
         <h1>Babysitter Management</h1>
         <div className="page-actions">
