6a92ab45dd638a10b543a8af96ece33fcd7ad5c9
Absolomjr <145746728+Absolomjr@users.noreply.github.com>
2025-05-18 21:36:26 +0300

    back

--- daystar_backend/server.js
+++ daystar_backend/server.js
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-Rate limiting
+// Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100 // limit each IP to 100 requests per windowMs
