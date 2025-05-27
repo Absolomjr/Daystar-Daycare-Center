540f688bac5d7a1fe5cbb4df9afbd69bb5e6f34e
Absolomjr <145746728+Absolomjr@users.noreply.github.com>
2025-05-27 15:23:03 +0300

    login 2

--- daystar_frontend/src/services/authService.js
+++ daystar_frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import api from './api';
 
-async function for the login, register, and other auth - related actions
+// async function for the login, register, and other auth - related actions
 export const authService = {
   async login(email, password) {
     const response = await api.post('/auth/login', { email, password });
