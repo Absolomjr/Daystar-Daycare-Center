b54e3ac66cdb9580991caa00400548e1f3016230
Absolomjr <145746728+Absolomjr@users.noreply.github.com>
2025-06-18 11:05:27 +0300

    routes again

--- daystar_backend/routes/attendance.js
+++ daystar_backend/routes/attendance.js
@@ -6,7 +6,7 @@ const roleCheck = require('../middleware/roleCheck');
 
 router.use(auth);
 
-Routes accessible by both the managers and the babysitters
+// Routes accessible by both the managers and the babysitters
 router.post('/mark', roleCheck(['manager', 'babysitter']), attendanceController.markAttendance);
 router.get('/history', roleCheck(['manager', 'babysitter']), attendanceController.getAttendanceHistory);
 
